7ab57b41997c0da641b397d11f5e80451a8f104d
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-01-21 22:16:04 +0200

    create DB, user auth and logout

--- website/views.py
+++ website/views.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, render_template
 import pandas as pd
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/')
+@login_required
 def home():
-    data = pd.read_csv('https://dev.hsl.fi/citybikes/od-trips-2021/2021-05.csv')
-    return render_template("home.html", text = data['Return station name'][1])
+    data = pd.read_csv(
+        'https://dev.hsl.fi/citybikes/od-trips-2021/2021-05.csv')
+    return render_template("home.html", text=data['Return station name'][1])
