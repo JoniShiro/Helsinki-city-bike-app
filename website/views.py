e5370bd7cf6a263e1d6d40fb8f44859c185d5adc
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-01-22 00:56:47 +0200

    hide navbar elements when user loggedIn

--- website/views.py
+++ website/views.py
@@ -8,6 +8,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 @login_required
 def home():
-    data = pd.read_csv(
-        'https://dev.hsl.fi/citybikes/od-trips-2021/2021-05.csv')
-    return render_template("home.html", text=data['Return station name'][1])
+    # data = pd.read_csv(
+    #     'https://dev.hsl.fi/citybikes/od-trips-2021/2021-05.csv')
+    # text = data['Return station name'][1]
+    return render_template("home.html", user=current_user)
